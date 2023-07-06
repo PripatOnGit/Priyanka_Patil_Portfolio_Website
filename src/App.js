af8f6ee69cbf27c33dd8a881ee28093e13ccd18a
PripatOnGit <patil.priyu26@gmail.com>
2023-07-06 16:59:55 +0530

    Portfolio created

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Navbar from "./components/Navbar";
+import Skills from "./components/Skills";
+import Testimonials from "./components/Testimonials";
+import Projects from "./components/Projects";
+
+export default function App() {
+  return (
+    <main className="text-gray-400 bg-gray-900 body-font">
+      <About />
+      
+    </main>
+  );
+}
---
+++ src/components/Navbar.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Navbar(){
+    return(
+        <main>
+            <h1>Hello</h1>
+        </main>
+    );
+
+}
